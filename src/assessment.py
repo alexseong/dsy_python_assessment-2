49da59270045e51f418224f6fd6ca065b74d2ae2
Karen Ryu <karenryu@Karens-MacBook.local>
2019-02-19 18:03:30 -0600

    Karen Ryu

--- src/assessment.py
+++ src/assessment.py
@@ -1,9 +1,31 @@
 '''
-Write your code under the commented(#) question. 
+Write your code under the commented(#) question.
 '''
 
 ### Task 1
 
+first = input('Enter first number: ')
+first = int(first)
 
+second = input('Enter second number: ')
+second = int(second)
+
+print(first+second)
+print(first-second)
+print(first*second)
 
 ### Task 2
+
+num = input('Enter a number: ')
+num = int(num)
+
+if num%2 == 1:
+    print('Weird')
+elif num in range (2,6): #including 2 but not 6 >>> 2,3,4,5
+    print('Not Weird')
+elif num in range (6,21):
+    print('Weird')
+elif num in range (21,101):
+    print('Not Weird')
+else:
+    print('Weird')
